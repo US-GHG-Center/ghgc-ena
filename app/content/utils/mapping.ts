bcf831abe9352f2268e3367b917682e5285a5aad
Shwethah <h.shwetha53@gmail.com>
2025-10-07 10:09:09 -0500

    Mapping as a single dictionary

--- app/content/utils/mapping.ts
+++ app/content/utils/mapping.ts
@@ -1,20 +1,20 @@
 export function mapDrupalToMdxIds() {
-  return [
-    { "nid": "111", "id": "blackmarble-radiance-daygrid-v2nrt" },
-    { "nid": "91",  "id": "blueflux-ghgflux-daygrid-v1" },
-    { "nid": "55",  "id": "ct-ch4-monthgrid-v2023" },
-    { "nid": "74",  "id": "eccodarwin-co2flux-monthgrid-v5" },
-    { "nid": "132", "id": "epa-ch4emission-yeargrid-v2express" },
-    { "nid": "41",  "id": "gedi-agb-grid-v2.1" },
-    { "nid": "116", "id": "geos-ghg-daygrid-vPRE" },
-    { "nid": "253", "id": "gosat-based-ch4budget-yeargrid-v1" },
-    { "nid": "47",  "id": "gra2pes-ghg-monthgrid-v1" },
-    { "nid": "128", "id": "lpjeosim-wetlandch4-grid-v1" },
-    { "nid": "67",  "id": "micasa-carbonflux-grid-v1" },
-    { "nid": "56",  "id": "oco2geos-co2-daygrid-v10r" },
-    { "nid": "129", "id": "oco2-mip-co2budget-yeargrid-v1"},
-    { "nid": "130", "id": "odiac-ffco2-monthgrid-v2024"},
-    { "nid": "131", "id": "sedac-popdensity-yeargrid5yr-v4.11"},
-    { "nid": "314", "id": "vulcan-ffco2-yeargrid-v4"}
-  ];
+  return { 
+      "111": "blackmarble-radiance-daygrid-v2nrt",
+      "91" : "blueflux-ghgflux-daygrid-v1",
+      "55" : "ct-ch4-monthgrid-v2023",
+      "74" : "eccodarwin-co2flux-monthgrid-v5",
+      "132": "epa-ch4emission-yeargrid-v2express",
+      "41" : "gedi-agb-grid-v2.1",
+      "116": "geos-ghg-daygrid-vPRE",
+      "253": "gosat-based-ch4budget-yeargrid-v1",
+      "47" : "gra2pes-ghg-monthgrid-v1",
+      "128": "lpjeosim-wetlandch4-grid-v1",
+      "67" : "micasa-carbonflux-grid-v1",
+      "56" : "oco2geos-co2-daygrid-v10r",
+      "129": "oco2-mip-co2budget-yeargrid-v1",
+      "130": "odiac-ffco2-monthgrid-v2024",
+      "131": "sedac-popdensity-yeargrid5yr-v4.11",
+      "314": "vulcan-ffco2-yeargrid-v4"
+    };
 }
